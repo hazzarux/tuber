ddfab8d8868081155b833f4b05b3dfe6932dc8fc
Yigit Ozkan <yigitozkan2804@gmail.com>
2011-03-20 00:06:10 +0100

    fixed downloader bash script

--- youtube_downloader.sh
+++ youtube_downloader.sh
@@ -2,6 +2,7 @@
 
 echo 'Type the filename: '
 read filename
-$edited="${filename}_edited.txt"
-#filename="18032011_edited.txt"
+suffix="_edited.txt"
+edited="$filename$suffix"
+# filename="18032011_edited.txt"
 sudo youtube-dl -i -l -f 5 -a "$edited"
