44f808c3e7c2932e0f9e72cb2c3dd8d9c554bfa1
Yigit Ozkan <yigitozkan2804@gmail.com>
2011-04-10 12:21:09 +0300

    renamed /bin/ directory to /src/

---
+++ src/youtube_downloader.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#This program is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+sudo apt-get install youtube-dl
+
+echo 'Type the filename: '
+read filename
+suffix="_edited.txt"
+edited="$filename$suffix"
+# filename="18032011_edited.txt"
+sudo youtube-dl -i -l -f 5 -a "$edited"
